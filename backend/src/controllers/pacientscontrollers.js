1d28976c3eb4aaa5154537773feb3612d37f3db9
Luis Gustavo F Freire <holyfreire@gmail.com>
2023-07-10 23:20:22 -0300

    ultima da noite

--- backend/src/controllers/pacientscontrollers.js
+++ backend/src/controllers/pacientscontrollers.js
@@ -1,9 +1,14 @@
-const getAll = (request, response) => {
-    return response.status(200).json({ message: "controller ta tudo certo"})
+const pacientsmodel = require("../models/pacientsModel");
 
-}
+const getAll = async (request, response) => {
+    
+    const  pacients = await pacientsmodel.getAll();
+
+    return response.status(200).json(pacients);
+
+};
 
 
 module.exports = {
     getAll
-}
+};
--- backend/src/models/pacientsModel.js
+++ backend/src/models/pacientsModel.js
@@ -1,10 +1,10 @@
-const connection = require("./connection")
+const connection = require("./connection");
 
 const getAll = async () => {
     const patients = await connection.execute("select *  FROM  patients");
     return patients;
 };
 
-modele.exports = {
+module.exports = {
     getAll
-}
+};
