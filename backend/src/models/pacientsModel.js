cdfb02700252c4dea7fdace1c14dbab8227cd246
Luis Gustavo F Freire <holyfreire@gmail.com>
2023-07-21 17:59:09 -0300

    CONFG

--- backend/src/models/pacientsModel.js
+++ backend/src/models/pacientsModel.js
@@ -13,15 +13,6 @@ const newPacient = async (name , age , gender , address, phone, email, admission
 
     // Constantes para os nomes das colunas da tabela Patients
 
-    const name = 'name';
-    const age = 'age';
-    const gender = 'gender';
-    const address = 'address';
-    const phone = 'phone';
-    const email = 'email';
-    const admissionDate = 'admissionDate';
-    const dischargeDate = 'dischargeDate';
-    const diagnosis = 'diagnosis';
     const query = "INSERT INTO Patients (name, age, gender, address, phone, email, admission_date, discharge_date, diagnosis, doctor_id, hospital_id ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
 
